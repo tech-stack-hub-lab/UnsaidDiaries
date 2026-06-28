df307cb2472a588df958eb60e19bc563aa7c135a
tech-stack-hub-lab <533702@waes.ac.uk>
2026-06-28 13:20:34 +0100

    implement view homepage to show posts

--- diaries/views.py
+++ diaries/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
+# from django.http import HttpResponse
 
 # Create your views here.
-def my_diaries(request):
-    return HttpResponse("Hello, blog!")
+# def my_diaries(request):
+#     return HttpResponse("Hello, blog!")
+class PostList(generic.ListView):
+    model = Post
--- unsaiddiaries/urls.py
+++ unsaiddiaries/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from diaries.views import my_diaries
+from django.urls import path, include
+# from diaries.views import my_diaries
 
 urlpatterns = [
-path('admin/', admin.site.urls),
-path('diaries/', my_diaries, name='diaries'),
+    path('admin/', admin.site.urls),
+    # path('diaries/', my_diaries, name='diaries'),
+    path("", include("diaries.urls"), name="diaries-urls"),
 ]
